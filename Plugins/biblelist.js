808d4d63f9e429c4f0a87bae6c948fe61d15928c
SilverTosh <silvestermigwi76@gmail.com>
2025-04-23 13:41:02 +0300

    Update biblelist.js

--- Plugins/biblelist.js
+++ Plugins/biblelist.js
@@ -89,7 +89,7 @@ cmd({
 `;
 
         // Remplacer ce lien par l'URL de l'image que tu m'enverras
-        const imageUrl = "https://i.ibb.co/nMT095YD/kingvon-xmd.jpg"; // Remplace "TON_LIEN_IMAGE_ICI" par ton lien d'image
+        const imageUrl = "https://files.catbox.moe/mtvyj5.jpg"; // Remplace "TON_LIEN_IMAGE_ICI" par ton lien d'image
 
         // Vérifier si le message de la commande est correctement reçu
         if (!m.chat) {
